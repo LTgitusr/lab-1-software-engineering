3e00dfe0f875c99d588d12b08a386c7298ac5fa3
DT <d.t8796@gmail.com>
2026-04-30 15:16:47 +0300

    Updates
    implemented switchScenes + implemented LoginController + updated LoginApplication (HelloApplication) + updated welcome.fxml + passed test

--- src/main/java/lab2/lab2softwareengineering/Controllers/LoginController.java
+++ src/main/java/lab2/lab2softwareengineering/Controllers/LoginController.java
@@ -2,10 +2,14 @@ package lab2.lab2softwareengineering.Controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+
+import lab2.lab2softwareengineering.UsersApp;
+import lab2.lab2softwareengineering.Utilities.switchScenes;
+
+import java.io.IOException;
 
 public class LoginController {
 
@@ -13,17 +17,38 @@ public class LoginController {
     private Label errorLabel;
 
     @FXML
-    private Button loginButton;
-
-    @FXML
     private PasswordField passwordField;
 
     @FXML
     private TextField usernameField;
 
-    @FXML
-    void onLoginButtonClick(ActionEvent event) {
+    private UsersApp usersApp;
 
+    @FXML
+    public void initialize()
+    {
+        usersApp = new UsersApp("users.txt");
+        errorLabel.setText("");
     }
 
+    @FXML
+    void onLoginButtonClick(ActionEvent event)
+    {
+        String username = usernameField.getText();
+        String password = passwordField.getText();
+
+        if(usersApp.isValid(username, password))
+        {
+            try
+            {
+                switchScenes.switchScene(usernameField, "welcome.fxml");
+            }
+            catch(IOException e)
+            {
+                errorLabel.setText("Couldn't load the next screen");
+            }
+        }
+        else
+            errorLabel.setText("user or password do not match");
+    }
 }
--- src/main/java/lab2/lab2softwareengineering/Utilities/switchScenes.java
+++ src/main/java/lab2/lab2softwareengineering/Utilities/switchScenes.java
@@ -1,4 +1,20 @@
 package lab2.lab2softwareengineering.Utilities;
 
-public class switchScenes {
+import java.io.IOException;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class switchScenes
+{
+    public static void switchScene(Node node, String fxmlFile) throws IOException
+    {
+        FXMLLoader loader = new FXMLLoader(switchScenes.class.getResource("/lab2/lab2softwareengineering/" + fxmlFile));
+
+        Scene scene = new Scene(loader.load());
+        Stage stage = (Stage) node.getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
 }
