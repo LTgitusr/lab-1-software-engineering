a28f25c8d31f4124f15fd4e48196b9e37b3bc625
DT <d.t8796@gmail.com>
2026-05-09 20:56:05 +0300

    Added enum for different login outcomes and updated UsersApp

---
+++ src/main/java/lab2/lab2softwareengineering/LoginStatus.java
@@ -0,0 +1,5 @@
+package lab2.lab2softwareengineering;
+
+public enum LoginStatus {
+    VALID, INVALID, BLOCKED
+}
--- src/main/java/lab2/lab2softwareengineering/UsersApp.java
+++ src/main/java/lab2/lab2softwareengineering/UsersApp.java
@@ -9,10 +9,15 @@ public class UsersApp
 {
     private ArrayList<User> users;
 
-    public UsersApp(String filename)
+    private int maxFailedLogins;
+    private int loginTimeout;
+
+    public UsersApp(String filename, int maxFailedLogins, int loginTimeout)
     {
         users = new ArrayList<>();
         readUsers(filename);
+        this.maxFailedLogins = maxFailedLogins;
+        this.loginTimeout = loginTimeout;
     }
     //method for reading users from a file and saving them in the class's user list
     private void readUsers(String filename)
@@ -47,14 +52,92 @@ public class UsersApp
             System.out.println("file not found");
         }
     }
-    //boolean method for checking if the username and the password entered are matching
-    public boolean isValid(String username, String password)
+
+    private User findUser(String username)
     {
         for (User user : users)
         {
-            if (user.getUsername().equals(username) && user.getPassword().equals(password))
-                return true;
+            if (user.getUsername().equals(username))
+                return user;
         }
-        return false;
+        return null;
+    }
+
+    private boolean failedAttemptsUpdateThreaded(User user)
+    {
+        final boolean[] gotBlocked = new boolean[1];
+        Thread failedAttempt = new Thread(() ->
+        {
+           user.addFailedAttempt();
+           if(user.getFailedLoginAttempts() >= maxFailedLogins)
+           {
+               user.block();
+               gotBlocked[0] = true;
+               Thread timeout = new Thread(() ->
+               {
+                   try
+                   {
+                       Thread.sleep(1000L * loginTimeout);
+                   }
+                   catch(InterruptedException e)
+                   {
+                       System.out.println("Timeout was interrupted");
+                   }
+                   user.unblock();
+               });
+               timeout.start();
+           }
+        });
+        failedAttempt.start();
+
+        try
+        {
+            failedAttempt.join();
+        }
+        catch(InterruptedException e)
+        {
+            System.out.println("Failed attempt was interrupted");
+        }
+        return gotBlocked[0];
+    }
+
+    private boolean blockCheckThreaded(User user)
+    {
+        final boolean[] blocked = new boolean[1];
+        Thread blockCheck = new Thread(() ->
+        {
+            blocked[0] = user.isBlocked();
+        });
+        blockCheck.start();
+        try
+        {
+            blockCheck.join();
+        }
+        catch(InterruptedException e)
+        {
+            System.out.println("Block check was interrupted");
+        }
+        return blocked[0];
+    }
+
+    public LoginStatus login(String username, String password)
+    {
+        User user = findUser(username);
+
+        if(user == null)
+            return LoginStatus.INVALID;
+
+        if(!user.getPassword().equals(password))
+        {
+            if(failedAttemptsUpdateThreaded(user))
+                return LoginStatus.BLOCKED;
+            return LoginStatus.INVALID;
+        }
+        if(blockCheckThreaded(user))
+        {
+            return LoginStatus.BLOCKED;
+        }
+        user.resetFailedLoginAttempts();
+        return LoginStatus.VALID;
     }
 }
